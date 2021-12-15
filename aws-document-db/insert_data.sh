fcf7803ce14ba1820995d66631497aec2527efe3
adoc-github <65012009+adoc-github@users.noreply.github.com>
2021-12-15 11:02:30 +0900

    Create insert_data.sh

---
+++ aws-document-db/insert_data.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+ENDPOINT="$1"
+USER="$2"
+PASSWORD="$3"
+DB="$4"
+COLLECTION="$5"
+DATA="$6"
+OS=`cat /etc/os-release`
+
+# install mongoimport cli tool
+echo 'Install MongoDB'
+apt-get update -y
+apt-get install gnupg -y
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+apt-get update -y
+apt-get install -y mongodb-org
+echo "mongodb-org hold" | dpkg --set-selections
+echo "mongodb-org-server hold" | dpkg --set-selections
+echo "mongodb-org-shell hold" | dpkg --set-selections
+echo "mongodb-org-mongos hold" | dpkg --set-selections
+echo "mongodb-org-tools hold" | dpkg --set-selections
+
+# dump inputs
+echo "Enpoint: $ENDPOINT"
+echo "User: $USER"
+echo "Password: ****"
+echo "DB Name: $DB"
+echo "Collection Name: $COLLECTION"
+echo "Data: $DATA"
+
+echo $OS
+
+# wait until cluster endpoint is listining
+apt-get install netcat -y
+timeout=600
+wait_interval=5
+for (( c=0 ; c<$timeout ; c=c+$wait_interval ))	
+do
+    # check if enpoint is listening
+    nc -z -w1 $ENDPOINT 27017
+    status=$?
+    if [ $status -ne 0 ]; then
+        # not listening yet, waiting
+        let remaining=$wait_sec-$c
+        echo "Endpoint $ENDPOINT is not listening on port 27017 yet, sleeping for $wait_interval. Remaining timeout is $remaining seconds."
+        unset status  # reset the $status var
+        
+        sleep $wait_interval
+    else
+        # listening, exit loop
+        echo "Endpoint is listening on port 27017, exiting wait loop"
+        break
+    fi
+done
+
+# load data to mongodb endpoint
+echo "$DATA" | mongoimport -h=$ENDPOINT -u=$USER -p=$PASSWORD -d=$DB -c=$COLLECTION --jsonArray
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Error importing data"
+else
+    echo "Imported data successfully"
+fi
+exit $retVal
